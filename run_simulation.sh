23852da1e7fbcc11a1943e3048d3aa2901113190
calvong <calvongchi@gmail.com>
2021-09-16 11:49:20 +1000

    Fix moveit_robot shell not showing in run_sim.sh

--- run_simulation.sh
+++ run_simulation.sh
@@ -2,6 +2,8 @@
 echo "Starting Rosbridge"
 gnome-terminal --tab -- roslaunch rosbridge_server rosbridge_websocket.launch 
 sleep 1
+echo "Starting MoveIt"
+gnome-terminal --tab -- roslaunch panda_moveit_config demo.launch
 echo "Starting ROS nodes"
 gnome-terminal --tab -- roslaunch hrca_decision cws_nodes.launch
 read
